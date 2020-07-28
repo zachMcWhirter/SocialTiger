5c56336f48e7c5fb06a6f87a462f7cbb93502c33
Zach McWhirter <zmac7811@gmail.com>
2020-07-27 22:19:38 -0500

    created empty components for AppViews, Home

--- src/components/SocialTiger.js
+++ src/components/SocialTiger.js
@@ -9,6 +9,7 @@ const SocialTiger = () => {
   return (
     <>
      <Login/>
+     <FolderCard/>
     </>
   );
 };
--- src/components/folder/FolderCard.js
+++ src/components/folder/FolderCard.js
@@ -1,21 +1,19 @@
 import React from "react";
 import FolderImage from "./folder-image.png"
+import "./FolderCard.css"
 
 const FolderCard = () => {
-   
+
     return (
         <div className="card">
-        <div className="card-content">
-          <picture>
-            <img src={FolderImage} alt="folder" />
-            <h3>
-            Name: <span className="card-foldername">Funny Stuff</span>
-          </h3>
-          </picture>
-          
-          <p>Breed: Poodle</p>
+            <div className="card-content">
+                <div className="img-container">
+                    
+                        <img src={FolderImage} alt="folder" />
+                        <div id="text" className="card-foldername">Funny Stuff</div>
+                </div>
+            </div>
         </div>
-      </div>
     )
 }
 
