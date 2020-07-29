62474745b74be2e8a0ac89308786b246bc3b76cd
Zach McWhirter <zmac7811@gmail.com>
2020-07-29 15:58:45 -0500

    in FolderForm.js - add a function and return a form  that will allow the user to create a new folder

---
+++ src/components/folder/FolderForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import FolderManager from "../../modules/FolderManager";
+
+const FolderForm = (props) => {
+    const [folder, setFolder] = useState({
+        folderName: "",
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleFieldChange = e => {
+        const stateToChange = { ...folder };
+        stateToChange[e.target.id] = e.target.value;
+        setFolder(stateToChange);
+    };
+
+    const createNewFolder = e => {
+        e.preventDefault();
+        if (folder.folderName === "") {
+            window.alert("Please input a folder name");
+        } else {
+            setIsLoading(true);
+            // Create the folder and redirect user to folder list
+            folderManager.post(folder)
+                .then(() => props.history.push("/folders"));
+        }
+    };
+
+    return (
+        <>
+            <form>
+                <fieldset>
+                    <div className="formgrid">
+                        <input
+                            type="text"
+                            required
+                            onChange={handleFieldChange}
+                            id="name"
+                            placeholder="FolderName"
+                        />
+                        <div className="alignRight">
+                            <button
+                                type="button"
+                                disabled={isLoading}
+                                onClick={createNewFolder}
+                            >Submit</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </>
+    );
+};
+
+export default FolderForm;
