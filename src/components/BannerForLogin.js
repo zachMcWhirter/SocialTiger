5d956fa40c7fe15f1ac32730ffc22818bb55057e
Zach McWhirter <zmac7811@gmail.com>
2020-08-02 16:25:35 -0500

    switching branches

--- src/components/BannerForLogin.js
+++ src/components/BannerForLogin.js
@@ -5,11 +5,11 @@ const BannerForLogin = (props) => {
 
     return (
         <>
-            
                 <div className="h1-container">
                     <h1>SOCIAL TIGER</h1>
                 </div>
                 <div>
+                    
                     <img 
                         className="logo" 
                         src="https://res.cloudinary.com/lordargyle22/image/upload/v1596292629/socialTiger/SocialTiger_bwtvfg.png" />
