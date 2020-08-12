291af7da8c6241d0f6c919321767886644981906
Zach McWhirter <zmac7811@gmail.com>
2020-08-12 12:55:50 -0500

    added user instructions to the home page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,7 +8,23 @@ const Welcome = (props) => {
 
             <div className="welcome-container">
                 <div className="h2-container">
-                    <h2>Welcome {props.user.email}</h2>
+                    <h2>Welcome {props.user.email}!</h2>
+                    <p className="welcome-instructions">
+                        New to Social Tiger? Here's how to get started!
+                    </p>    
+                            <ul className="instructions">
+                                <li>Click the "Folders" button on the NavBar</li> 
+                                <li>Click the "Add New Folder" button</li>
+                                <li>Give your folder a name and click "Submit"</li>
+                                <li>Open your new folder by clicking on it</li>
+                                <li>Click the "Add New Image" button</li>
+                                <li>Assign your image a folder, a name,  <br></br>
+                                    and a description</li>
+                                <li>Choose a file to upload and click "Submit"</li> 
+                            </ul>
+
+
+                    
                 </div>
             </div>
         </>
