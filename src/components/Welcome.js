f0228dda7f772b778a5afc1085a9ac91200a643f
Zach McWhirter <zmac7811@gmail.com>
2020-08-14 13:06:48 -0500

    explain social tiger on homepage

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,13 +5,13 @@ const Welcome = (props) => {
 
     return (
         <>
-
-            <div className="welcome-container">
-                <div className="h2-container">
-                    <h2>Welcome {props.user.email}!</h2>
-                    <p className="welcome-instructions">
-                        New to Social Tiger? Here's how to get started!
-                    </p>    
+            <div className="under-nav-over-randomizer">
+                <div className="welcome-container">
+                    <div className="h2-container">
+                        <h2>Welcome {props.user.email}!</h2>
+                        <p className="welcome-instructions">
+                            New to Social Tiger? Here's how to get started!
+                        </p>    
                             <ul className="instructions">
                                 <li>Click the "Folders" button on the NavBar</li> 
                                 <li>Click the "Add New Folder" button</li>
@@ -22,11 +22,22 @@ const Welcome = (props) => {
                                     and a description</li>
                                 <li>Choose a file to upload and click "Submit"</li> 
                             </ul>
+                    </div>
+                    <div className="social-tiger-definition">
+                        TOP DEFINITION
+                        <h4 id="ST-definition-h4">Social Tiger</h4>
+                        <p id="ST-definition-body">That seemimgly quiet fellow who <br/>"crouches in the brush" of a conversation,<br/>
+                        standing in the background of the crowd,<br/> waiting patiently for the perfect moment 
+                        to<br/> say something hilarious.<br/><br/>
 
+                        synonym: Opportunistic Comedian<br/>
+                        </p>
+                        <h6 id="urban-dictionary">urban dictionary</h6>
+                        {/* <img id="ST-definition" src="https://res.cloudinary.com/lordargyle22/image/upload/v1597160968/socialTiger/Screen_Shot_2020-07-23_at_6.52.47_PM_qmkgy4.png" /> */}
 
-                    
+                    </div>
                 </div>
-            </div>
+            </div>    
         </>
     )
 }
